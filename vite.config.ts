a6a7243c73ca6950481b8c7c6dccfd60696d7c24
Tenma <tenma@osxt.com>
2024-07-18 11:20:26 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/CI-CD-Ynov/",
   server: {
     host: true,
   },
