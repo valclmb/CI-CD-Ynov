7cf6e8285556201fc2926b943c97aeae1f044dcc
Tenma <tenma@osxt.com>
2024-07-18 11:01:35 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin("all")],
+  plugins: [react()],
   server: {
     host: true,
   },
