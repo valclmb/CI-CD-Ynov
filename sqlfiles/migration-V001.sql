d25cb255dddf4f01febde8b2b68d0229bf6cbf6e
Tenma <tenma@osxt.com>
2024-08-01 12:29:17 +0200

    delete server

--- sqlfiles/migration-V001.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE ynov_ci;
--- sqlfiles/migration-V003.sql
+++
@@ -1,6 +0,0 @@
-use ynov_ci;
-
-INSERT INTO users (lastName, firstName, email, password, birthDate, city, postalCode)
-VALUES ('Doe', 'John', 'johndoe@example.com', 'password123', '1990-01-01', 'New York', '10001');
-
-
